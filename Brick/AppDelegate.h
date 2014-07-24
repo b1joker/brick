ac12db0a874888c145d910b085b7ef0e91593c2e
Lam Nguyen <nhlam.225@gmail.com>
2014-07-24 08:34:13 +0700

    Initial Commit

---
+++ Brick/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Brick
+//
+//  Created by Gin on 7/24/14.
+//  Copyright (c) 2014 Nguyễn Huỳnh Lâm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Brick/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Brick
+//
+//  Created by Gin on 7/24/14.
+//  Copyright (c) 2014 Nguyễn Huỳnh Lâm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
